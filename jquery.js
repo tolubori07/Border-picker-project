971d6c16ccf9e003c48f43eafd075fc4479cdcd0
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2023-06-19 00:04:31 +0100

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,18 +1,24 @@
-$(document).ready(
-    $('#btlactivator').click(function () {
+$(document).ready(function () {
+   $('#btl').change(function (e) { 
+    e.preventDefault();
     const btl = ($('#btl').val()+'px')
     $('#text').css('border-top-left-radius',btl)
-}),
-$('#btractivator').click(function () {
-    const btr = ($('#btr').val()+'px')
-    $('#text').css('border-top-right-radius',btr)
-}),
-$('#bblactivator').click(function () {
+   });
+   $('#bbr').change(function (e) { 
+    e.preventDefault();
+    const bbr  = ($('#bbr').val()+'px')
+    $('#text').css('border-bottom-right-radius',bbr)
+   });
+   $('#bbl').change(function (e) { 
+    e.preventDefault();
     const bbl = ($('#bbl').val()+'px')
     $('#text').css('border-bottom-left-radius',bbl)
-}),
-$('#bbractivator').click(function () {
-    const bbr = ($('#bbr').val()+'px')
-    $('#text').css('border-bottom-right-radius',bbr)
+   });
+   $('#btr').change(function (e) { 
+    e.preventDefault();
+    const btr = ($('#btr').val()+'px')
+    $('#text').css('border-top-right-radius',btr)
+   });
 })
-)
+
+
